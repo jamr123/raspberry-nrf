5d7d6664bb4fb6aa299e297183858ac3b1415e87
Jorge Meza <jorge.meza.rdz@gmail.com>
2019-12-12 23:53:58 -0600

    init

--- lib_nrf24.py
+++ lib_nrf24.py
@@ -186,14 +186,14 @@ class NRF24:
         # On rpi, only bus 0 is supported here, not bus 1 of the model B plus
         self.GPIO = gpio   # the GPIO module
         self.spidev = spidev # the spidev object/instance
-        self.channel = 0x64
+        self.channel = 76
         self.data_rate = NRF24.BR_1MBPS
         self.wide_band = False # 2Mbs data rate in use?
         self.p_variant = False # False for RF24L01 and true for RF24L01P (nrf24l01+)
         self.payload_size = 5 #*< Fixed size of payloads
-        self.ack_payload_available = True #*< Whether there is an ack payload waiting
-        self.dynamic_payloads_enabled = True #*< Whether dynamic payloads are enabled.
-        self.ack_payload_length = 32 #*< Dynamic size of pending ack payload.
+        self.ack_payload_available = False #*< Whether there is an ack payload waiting
+        self.dynamic_payloads_enabled = False #*< Whether dynamic payloads are enabled.
+        self.ack_payload_length = 5 #*< Dynamic size of pending ack payload.
         self.pipe0_reading_address = None #*< Last address set on pipe 0 for reading.
 
     def ce(self, level):
@@ -370,7 +370,7 @@ class NRF24:
         # ce_pin is for the rx=listen or tx=trigger pin on RF24 (they call that ce !!!)
         # CE optional (at least in some circumstances, eg fixed PTX PRX roles, no powerdown)
         # CE seems to hold itself as (sufficiently) HIGH, but tie HIGH is safer!
-        
+        self.spidev.max_speed_hz = 4000000
         self.spidev.open(0, csn_pin)
         self.ce_pin = ce_pin
 
