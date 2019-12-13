6dc85bff787c3e1e4124e35843674bb11e6849f5
Jorge Meza <jorge.meza.rdz@gmail.com>
2019-12-12 23:25:15 -0600

    init

--- nodo_01/nodo_01.ino
+++ nodo_01/nodo_01.ino
@@ -1,69 +1,23 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
+ 
+#include<SPI.h>
+#include<RF24.h>
+ 
+// ce, csn pins
 RF24 radio(9, 10);
-char receivedMessage[10];;
-String stringMessage = "";
-bool newData = false;
-
-const uint64_t TX = 0xAAAAAAAA01LL;
-const uint64_t RX = 0xAAAAAAAAFFLL;
-void setup(void)
-{
-  while (!Serial);
-  Serial.begin(9600);
-  printf_begin();
+ 
+void setup(void){
   radio.begin();
-  radio.setPALevel(RF24_PA_MIN);
-  radio.openWritingPipe(TX);
-  radio.openReadingPipe(0, RX);
-  radio.setDataRate( RF24_1MBPS );
-  radio.setAutoAck(1);
-  radio.enableAckPayload();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
   radio.enableDynamicPayloads();
-  radio.setRetries(15, 15);
-  radio.setChannel(0x64);
-  radio.printDetails(); 
-  radio.startListening();
-  Serial.println("inicio");
+  radio.powerUp();
+ 
 }
-
-void loop(void)
-{
-
-
-  getData();
-  showData();
-
-}
-
-
-void getData() {
-  if ( radio.available() ) {
-    radio.read( &receivedMessage, sizeof(receivedMessage) );
-    stringMessage = String(receivedMessage);
-    Serial.println("recive");
-    newData = true;
-  }
-}
-
-void showData() {
-  if (newData == true) {
-    Serial.println(stringMessage);
-    stringMessage = "";
-    newData = false;
-  }
-}
-
-int serial_putc( char c, FILE * ) 
-{
-  Serial.write( c );
-
-  return c;
-} 
-
-void printf_begin(void)
-{
-  fdevopen( &serial_putc, 0 );
+ 
+void loop(void){
+    const char text[] = "Hello World is awesome";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+ 
 }
